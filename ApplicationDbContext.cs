5323d19f71e06e1fa587b8452cb1598298815665
Asmo Korkiatupa <asmo.korkiatupa@digia.com>
2020-06-10 22:24:42 +0300

    Removed unused using statemets, added EOLs

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace netcore_api
+﻿namespace netcore_api
 {
     public class ApplicationDbContext
     {
--- Data/DataRepository.cs
+++ Data/DataRepository.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Data.SqlClient;
 using Dapper;
 using netcore_api.Data.Models;
