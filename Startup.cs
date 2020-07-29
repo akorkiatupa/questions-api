4ba27951bb3ccefae24012b38fd6d8a774135065
Asmo Korkiatupa <asmo.korkiatupa@digia.com>
2020-07-29 09:25:55 +0300

    Added whitelisting for localhost

--- Startup.cs
+++ Startup.cs
@@ -49,7 +49,7 @@ namespace netcore_api
             services.AddScoped<IDataRepository, DataRepository>();
 
             // Add Cross origin policy, accept everything from dev environment
-            services.AddCors(options => options.AddPolicy("CorsPolicy", builder => builder.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://127.0.0.1:3000").AllowCredentials()));
+            services.AddCors(options => options.AddPolicy("CorsPolicy", builder => builder.AllowAnyMethod().AllowAnyHeader().WithOrigins(new string[] { "http://127.0.0.1:3000", "http://localhost:3000" }).AllowCredentials()));
 
             services.AddSignalR();
             services.AddMemoryCache();
